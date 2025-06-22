8df0dcceef16c568597f1df1491254ce32c913ee
Philipp Wagner <pwag86@googlemail.com>
2025-06-22 18:11:39 +0200

    Fix Import Script

--- scripts/import.bat
+++ scripts/import.bat
@@ -3,7 +3,7 @@
 :: Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 :: OSM2PGSQL Executable Executable
-set OSM2PGSQL_EXECUTABLE=
+set OSM2PGSQL_EXECUTABLE="C:\Users\philipp\source\repos\bytefish\osm2pgsql-latest-x64\osm2pgsql-bin\osm2pgsql.exe"
 
 :: Parameters
 set OSM_STYLE="C:\Users\philipp\source\repos\bytefish\osm2pgsql-latest-x64\osm2pgsql-bin\default.style"
@@ -14,10 +14,7 @@ SET DB_PASSWD=postgis
 set DB_NAME=flinkjam
 set DB_HOST=localhost
 set DB_PORT=5432
-SET CACHE_FILE="./flat_nodes.cache
+SET CACHE_FILE="./flat_nodes.cache"
 
 :: Run the "kiota generate" Command
-%OSM2PGSQL_EXECUTABLE% -S %OSM_STYLE%^
-    -d %DB_NAME% -U postgis -W -H %DB_HOST% -P %DB_PORT%^
-    -s -C 2000 --flat-nodes=%CACHE_FILE% --hstore^
-    %OSM_PBF%
+%OSM2PGSQL_EXECUTABLE% -S %OSM_STYLE% -d %DB_NAME% -U postgis -W -H %DB_HOST% -P %DB_PORT% -s -C 2000 --flat-nodes=%CACHE_FILE% --hstore %OSM_PBF%
