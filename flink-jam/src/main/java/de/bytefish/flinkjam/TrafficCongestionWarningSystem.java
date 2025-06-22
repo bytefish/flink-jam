607b335ab847f1ed36b98dbc5ba4e4a9078493e4
Philipp Wagner <pwag86@googlemail.com>
2025-06-22 13:22:25 +0200

    Starting the Implementation with a WatermarkStrategy

--- flink-jam/src/main/java/de/bytefish/flinkjam/TrafficCongestionWarningSystem.java
+++ flink-jam/src/main/java/de/bytefish/flinkjam/TrafficCongestionWarningSystem.java
@@ -1,6 +1,10 @@
 package de.bytefish.flinkjam;
 
+import de.bytefish.flinkjam.models.RawTrafficEvent;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import java.time.Duration;
 
 public class TrafficCongestionWarningSystem {
 
@@ -8,6 +12,13 @@ public class TrafficCongestionWarningSystem {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         env.setParallelism(1);
+
+        // We define a WatermarkStrategy for all event streams. forBoundedOutOfOrderness is a common choice for
+        // real-time applications, allowing for some events to arrive out of order. We allow 5 seconds
+        // out-of-orderness.
+        WatermarkStrategy<RawTrafficEvent> rawEventWmStrategy = WatermarkStrategy
+                .<RawTrafficEvent>forBoundedOutOfOrderness(Duration.ofSeconds(5))
+                .withTimestampAssigner((event, recordTimestamp) -> event.getTimestamp());
 
         // TODO Implement it maybe?
 
