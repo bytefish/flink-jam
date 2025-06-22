92e40738b4837e7c22071a4d6c031cdb17323790
Philipp Wagner <pwag86@googlemail.com>
2025-06-22 17:31:00 +0200

    Fix Dockerfile

---
+++ scripts/import.bat
@@ -0,0 +1,23 @@
+@echo off
+
+:: Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+:: OSM2PGSQL Executable Executable
+set OSM2PGSQL_EXECUTABLE=
+
+:: Parameters
+set OSM_STYLE="C:\Users\philipp\source\repos\bytefish\osm2pgsql-latest-x64\osm2pgsql-bin\default.style"
+set OSM_PBF="C:\Users\philipp\Downloads\nordrhein-westfalen-250618.osm.pbf"
+
+SET DB_USER=postgis
+SET DB_PASSWD=postgis
+set DB_NAME=flinkjam
+set DB_HOST=localhost
+set DB_PORT=5432
+SET CACHE_FILE="./flat_nodes.cache
+
+:: Run the "kiota generate" Command
+%OSM2PGSQL_EXECUTABLE% -S %OSM_STYLE%^
+    -d %DB_NAME% -U postgis -W -H %DB_HOST% -P %DB_PORT%^
+    -s -C 2000 --flat-nodes=%CACHE_FILE% --hstore^
+    %OSM_PBF%
--- sql/02-create_tables.sql
+++ sql/02-create_tables.sql
@@ -2,16 +2,16 @@ DROP TABLE IF EXISTS road_segments;
 
 CREATE TABLE road_segments (
     osm_id BIGINT PRIMARY KEY, -- OSM IDs can be large, use BIGINT
-    osm_type text,  -- e.g., 'way' (from osm2pgsql)
-    road_type text,      -- e.g., 'Autobahn', 'Bundesstrasse', 'Stadtstrasse' (your classification)
-    name text,       -- Road name (optional, but useful for debugging)
-    ref text, -- Name of the Highway
+    osm_type TEXT,  -- e.g., 'way' (from osm2pgsql)
+    road_type TEXT,      -- e.g., 'Autobahn', 'Bundesstrasse', 'Stadtstrasse' (your classification)
+    name TEXT,       -- Road name (optional, but useful for debugging)
+    ref TEXT, -- Name of the Highway
     speed_limit_kmh INTEGER, -- Speed limit in km/h
     tags hstore, -- Tags
     geom GEOMETRY(LineString, 4326) -- The actual road geometry
 );
 
-CREATE INDEX idx_road_segments_id ON road_segments (id);
+CREATE INDEX idx_road_segments_id ON road_segments (osm_id);
 CREATE INDEX idx_road_segments_geom ON road_segments USING GIST(geom);
 CREATE INDEX idx_road_segments_geom_geography ON road_segments USING gist((geom::geography));
 
@@ -20,12 +20,12 @@ DROP TABLE IF EXISTS traffic_lights;
 -- Create the traffic_lights table
 CREATE TABLE traffic_lights (
     osm_id BIGINT PRIMARY KEY, -- OSM ID of the traffic light node
-    name VARCHAR(255),     -- Optional: Name of the intersection or road
+    name TEXT,     -- Optional: Name of the intersection or road
     is_pedestrian_crossing_light BOOLEAN DEFAULT FALSE, -- To distinguish car signals from pedestrian signals
-    tags,
+    tags hstore, -- We probably need the tags to get additional data. Very useful for debugging.
     geom GEOMETRY(Point, 4326) -- The geographical point of the traffic light
 );
 
-CREATE INDEX idx_traffic_lights_id ON traffic_lights (id);
+CREATE INDEX idx_traffic_lights_id ON traffic_lights (osm_id);
 CREATE INDEX idx_traffic_lights_geom ON traffic_lights USING GIST(geom);
 CREATE INDEX idx_traffic_lights_geom_geography ON traffic_lights USING gist((geom::geography));
